066b502091f14a63edac2bd988b721c29a6309e6
Joel Semczyszyn <joelsemczyszyn@gmail.com>
2016-05-14 15:32:25 -0700

    Gradient orientation fix

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ var bgCvs = bg.getContext("2d");
 
 // Gradients
 
-var bgGrd = bgCvs.createLinearGradient(0, 0, 1400, 400);
-bgGrd.addColorStop(1, "rgb(255,255,255)" );
-bgGrd.addColorStop(0.36, "rgb(234,234,234)" );
-bgGrd.addColorStop(0.3, "rgb(255,255,255)" );
-bgGrd.addColorStop(0, "rgb(242,242,242)" );
+var bgGrd = bgCvs.createLinearGradient(0, 0, 0, 400);
+bgGrd.addColorStop(0, "rgb(255,255,255)" );
+bgGrd.addColorStop(0.7, "rgb(234,234,234)" );
+bgGrd.addColorStop(0.76, "rgb(255,255,255)" );
+bgGrd.addColorStop(1, "rgb(242,242,242)" );
 
 // Operations 
 
