3e1f266fad10a96573abbb429e8ad3c7b3139381
Joel Semczyszyn <joelsemczyszyn@gmail.com>
2016-05-14 17:01:45 -0700

    Test button now works

--- main.js
+++ main.js
@@ -8,8 +8,9 @@
 // Variables 
 // ----------------------------------- //
 
-var bg = document.getElementById("background");
-var bgCvs = bg.getContext("2d");
+var bg = document.getElementById("background"),
+	bgCvs = bg.getContext("2d"),
+	testBtn = document.getElementById("test");
 
 // 
 // Canvas Work  
@@ -27,3 +28,31 @@ bgGrd.addColorStop(1, "rgb(242,242,242)" );
 
 bgCvs.fillStyle = bgGrd;
 bgCvs.fillRect(0, 0, 1400, 400);
+
+// 
+// Greensock Animation 
+// ------------------------------------- //
+
+// Timelines
+
+var	tl = new TimelineLite( {paused:true} );
+
+
+// Main Timeline
+
+tl.add( TweenMax.staggerFromTo(".slide1", 0.5, {opacity: 0, y:10}, {opacity: 1, y:0}, 0.2));
+/*	.staggerTo(".ys", 0.6, {opacity: 0, y:-10, delay:3}, 0.1)
+	.staggerFromTo(".st", 0.8, {opacity: 0, y:10}, {opacity: 1, y:0}, 0.2)
+	.staggerTo(".st", 0.6, {opacity: 0, y:-10, delay:3}, 0.1)
+	.staggerFromTo(".csn", 0.8, {opacity: 0, y:10}, {opacity: 1, y:0}, 0.2)
+	.staggerTo(".csn", 0.6, {opacity: 0, y:-10, delay:3}, 0.1)
+	.staggerTo(".bg1", 0.8, {opacity: 0}, 0.7)
+	.add(tlrestart); */
+
+//
+// Functions 
+// ------------------------------------ //
+
+function tlPlay() { tl.play(); }
+
+test.addEventListener("click", tlPlay);
